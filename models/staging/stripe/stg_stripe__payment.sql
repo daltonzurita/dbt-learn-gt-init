05a155a4e8eebcbd59b789bf14892a3ebc4df285
Dalton Zurita <andre_zurita96@outlook.com>
2026-06-29 23:57:49 -0500

    add sources

--- models/staging/stripe/stg_stripe__payment.sql
+++ models/staging/stripe/stg_stripe__payment.sql
@@ -3,6 +3,7 @@ orderid as order_id,
 paymentmethod as payment_method,
 status,
 amount/100 as amount,
-created as created_at
-from raw.stripe.payment
+created as created_at,
+_batched_at
+from {{source('stripe', 'payment')}}
 
